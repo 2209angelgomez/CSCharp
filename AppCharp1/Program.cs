5c633272a4591ba523ea50651dba489837bc5b1d
2209angelgomez <41799942+2209angelgomez@users.noreply.github.com>
2019-04-08 13:34:29 -0500

    j

---
+++ AppCharp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AppCharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 18;
+            string nombre;
+            var pelicula = "EndGame";
+            Console.Write("Introduzca su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine($"Bienvenido {nombre} tienes {edad} años");
+            Console.WriteLine($"Proximo estreno {pelicula} 25 de abril");
+            Console.ReadLine();
+        }
+    }
+}
